963ea7325f1053a301fe5e3a60a79059feb43636
cross <70271667+crosswind19@users.noreply.github.com>
2024-09-08 19:48:51 +0800

    Initial Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <div>Kah Xuan</div>
+            <div>
+                <a href="#home">Home</a>
+                <a href="#page1">Page1</a>
+                <a href="#page2">Page2</a>
+                <a href="#page3">Page3</a>
+            </div>
+            <button>Contact Me</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
