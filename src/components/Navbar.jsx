ddd6d5f1e5624edf75debdc54762d63c52261492
cross <70271667+crosswind19@users.noreply.github.com>
2024-09-09 22:40:28 +0800

    To be updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ const Navbar = () => {
     <nav className='bg-black text-white px-8 md:px-16 lg:px-24'>
         <div className='container py-2 flex justify-center md:justify-between items-center'>
             <div className='text-2xl font-bold hidden md:inline'>Kah Xuan</div>
-            <div className='space-x-6'>
+            {/* <div className='space-x-6'>
                 <a href="#home" className='hover:text-gray-400'>Home</a>
                 <a href="#page1" className='hover:text-gray-400'>Page1</a>
                 <a href="#page2" className='hover:text-gray-400'>Page2</a>
                 <a href="#page3" className='hover:text-gray-400'>Page3</a>
-            </div>
+            </div> */}
             <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Contact Me</button>
         </div>
     </nav>
