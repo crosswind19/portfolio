0f5ec20c1143ed11b85ba9e1c041e89e13d90f8b
cross <70271667+crosswind19@users.noreply.github.com>
2024-11-09 13:39:22 +0800

    Fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Experience from './components/Experience'
 import Projects from './components/Projects'
-import Contact from './components/Contact'
+// import Contact from './components/Contact'
 
 function App() {
   return(
@@ -14,7 +14,7 @@ function App() {
     <Hero />
     <About />
     <Experience />
-    {/* <Projects /> */}
+    <Projects />
     {/* <Contact /> */}
   </div>
   )
