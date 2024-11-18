bdb80311e664947a14fb8ec17b740b4694a88268
cross <70271667+crosswind19@users.noreply.github.com>
2024-11-19 00:10:23 +0800

    Filter, Sort and Validation done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,10 +11,17 @@ const Hero = () => {
             , Software Engineer 
         </h1>
         <p className='mt-4 text-lg text-gray-300'>I specialize in web application</p>
-        <div className='mt-8 space-x-4'>
-            <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Contact with me</button>
-            <button className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Resume</button>
-        </div>
+        <a href="https://www.linkedin.com/in/cheah-kah-xuan/" target="_blank" rel="noopener noreferrer">
+          <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>
+            Contact with me
+          </button>
+        </a>
+
+        <a href="https://your-resume-url.com" target="_blank" rel="noopener noreferrer">
+          <button className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>
+            Resume
+          </button>
+        </a>
     </div>
   )
 }
